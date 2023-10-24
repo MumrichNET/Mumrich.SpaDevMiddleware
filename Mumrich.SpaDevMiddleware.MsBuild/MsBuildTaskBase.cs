cbe95d1d59ebe6159fe4ee74099e41038c3db6b7
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-10-24 17:06:13 +0200

    wip.

--- Mumrich.SpaDevMiddleware.MsBuild/MsBuildTaskBase.cs
+++ Mumrich.SpaDevMiddleware.MsBuild/MsBuildTaskBase.cs
@@ -1,32 +1,12 @@
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
-using Mumrich.SpaDevMiddleware.Domain.Models;
 
 using MsBuildTask = Microsoft.Build.Utilities.Task;
 
 namespace Mumrich.SpaDevMiddleware.MsBuild
 {
-  public class AppSettings
-  {
-    public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new Dictionary<string, SpaSettings>();
-  }
-
   public abstract class MsBuildTaskBase : MsBuildTask
   {
-    private readonly JsonSerializerOptions _options;
-
-    protected MsBuildTaskBase()
-    {
-      _options = new JsonSerializerOptions();
-      _options.Converters.Add(new JsonStringEnumConverter());
-    }
-
-    public string AspNetCoreEnvironment { get; set; }
     public string CurrentDir { get; } = Directory.GetCurrentDirectory();
     protected bool IsWindows { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
@@ -34,35 +14,6 @@ namespace Mumrich.SpaDevMiddleware.MsBuild
     {
       var spaRootPathWithBackslashed = spaRootPath.Replace("/", "\\");
       return spaRootPathWithBackslashed.EndsWith("\\") ? spaRootPathWithBackslashed : $"{spaRootPathWithBackslashed}\\";
-    }
-
-    protected AppSettings TryReadAppSettings(string filePath)
-    {
-      if (filePath is null)
-      {
-        return new AppSettings();
-      }
-
-      var fileText = File.ReadAllText(filePath);
-      return JsonSerializer.Deserialize<AppSettings>(fileText, _options);
-    }
-
-    protected AppSettings TryReadDefaultAppSettingsFile()
-    {
-      var defaultAppSettingsFile = Directory
-        .GetFiles(CurrentDir, "appsettings.json")
-        .FirstOrDefault();
-
-      return TryReadAppSettings(defaultAppSettingsFile);
-    }
-
-    protected AppSettings TryReadEnvAppSettingsFile()
-    {
-      var envAppSettingsFile = Directory
-        .GetFiles(CurrentDir, $"appsettings.{AspNetCoreEnvironment}.json")
-        .FirstOrDefault();
-
-      return TryReadAppSettings(envAppSettingsFile);
     }
   }
 }
