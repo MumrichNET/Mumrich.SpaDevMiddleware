cbe8c3b1afedf8f2e2153ae16bbe9234212f97b5
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-10-24 13:45:18 +0200

    remove old web-host, add new vue-spa-app.

--- Mumrich.SpaDevMiddleware.Demo.WebHost/Program.cs
+++ Mumrich.SpaDevMiddleware.Demo.WebHost/Program.cs
@@ -1,15 +1,46 @@
-namespace Mumrich.SpaDevMiddleware.Demo.WebHost
+using Mumrich.SpaDevMiddleware.Domain.Contracts;
+using Mumrich.SpaDevMiddleware.Domain.Models;
+using Mumrich.SpaDevMiddleware.Domain.Types;
+using Mumrich.SpaDevMiddleware.Extensions;
+
+namespace Mumrich.SpaDevMiddleware.Demo.WebHost;
+
+internal class AppSettings : ISpaDevServerSettings
 {
-  public static class Program
+  public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new();
+  public string SpaRootPath { get; set; }
+  public bool UseParentObserverServiceOnWindows { get; set; }
+}
+
+public static class Program
+{
+  public static void Main(string[] args)
   {
-    public static void Main(string[] args)
+    var builder = WebApplication.CreateBuilder(args);
+    var appSettings = new AppSettings
     {
-      var builder = WebApplication.CreateBuilder(args);
-      var app = builder.Build();
+      SinglePageApps = new Dictionary<string, SpaSettings>()
+      {
+        {
+          "/",
+          new SpaSettings()
+          {
+            DevServerAddress = "http://localhost:3000/",
+            SpaRootPath = "Apps/vue-demo-app",
+            NodePackageManager = NodePackageManager.Npm
+          }
+        }
+      },
+      SpaRootPath = Directory.GetCurrentDirectory(),
+      UseParentObserverServiceOnWindows = false
+    };
 
-      app.MapGet("/", () => "Hello World!");
+    builder.RegisterSinglePageAppDevMiddleware(appSettings);
 
-      app.Run();
-    }
+    var app = builder.Build();
+
+    app.MapGet("/hello", () => "Hello World!");
+
+    app.Run();
   }
 }
