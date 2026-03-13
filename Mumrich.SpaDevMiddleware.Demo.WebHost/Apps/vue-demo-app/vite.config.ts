c064b747fa0303583a163dccbc986eb2d36090f3
Hannes Morgenthaler <hannes@morgethaler.ch>
2026-03-13 19:14:45 +0100

    migrate to vite+

--- Mumrich.SpaDevMiddleware.Demo.WebHost/Apps/vue-demo-app/vite.config.ts
+++ Mumrich.SpaDevMiddleware.Demo.WebHost/Apps/vue-demo-app/vite.config.ts
@@ -1,11 +1,84 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  fmt: {
+    "semi": false,
+    "singleQuote": true
+  },
+  lint: {
+    "plugins": [
+      "eslint",
+      "typescript",
+      "unicorn",
+      "oxc",
+      "vue"
+    ],
+    "env": {
+      "browser": true,
+      "builtin": true
+    },
+    "categories": {
+      "correctness": "error"
+    },
+    "ignorePatterns": [
+      "**/dist/**",
+      "**/dist-ssr/**",
+      "**/coverage/**"
+    ],
+    "rules": {
+      "@typescript-eslint/ban-ts-comment": "error",
+      "no-array-constructor": "error",
+      "@typescript-eslint/no-empty-object-type": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-namespace": "error",
+      "@typescript-eslint/no-require-imports": "error",
+      "@typescript-eslint/no-unnecessary-type-constraint": "error",
+      "@typescript-eslint/no-unsafe-function-type": "error"
+    },
+    "overrides": [
+      {
+        "files": [
+          "**/*.ts",
+          "**/*.tsx",
+          "**/*.mts",
+          "**/*.cts",
+          "**/*.vue"
+        ],
+        "rules": {
+          "constructor-super": "off",
+          "getter-return": "off",
+          "no-class-assign": "off",
+          "no-const-assign": "off",
+          "no-dupe-class-members": "off",
+          "no-dupe-keys": "off",
+          "no-func-assign": "off",
+          "no-import-assign": "off",
+          "no-new-native-nonconstructor": "off",
+          "no-obj-calls": "off",
+          "no-redeclare": "off",
+          "no-setter-return": "off",
+          "no-this-before-super": "off",
+          "no-undef": "off",
+          "no-unreachable": "off",
+          "no-unsafe-negation": "off",
+          "no-var": "error",
+          "no-with": "off",
+          "prefer-const": "error",
+          "prefer-rest-params": "error",
+          "prefer-spread": "error"
+        }
+      }
+    ],
+    "options": {
+      "typeAware": true,
+      "typeCheck": true
+    }
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
